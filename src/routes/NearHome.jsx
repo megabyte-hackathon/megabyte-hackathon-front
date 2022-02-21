8d5e026539ed980a459a5139c60bef31e8eb4a99
GyoHeonLee <ghl12078@gmail.com>
2022-02-22 00:09:48 +0900

    feat: make nearhome route

--- src/routes/NearHome.jsx
+++ src/routes/NearHome.jsx
@@ -1,1 +1,97 @@
+import styled from "styled-components";
+import "leaflet/dist/leaflet.css";
 
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { useSelector, useDispatch } from "react-redux";
+import { careerActions } from "../store/career";
+import { useState } from "react";
+
+const NearHome = () => {
+  return (
+    <MapBody>
+      <div>
+        <SelectedMap center={[37.564, 127.001]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+          />
+        </SelectedMap>
+        <MatZip>
+          <h1>!!회사이름!! 근처에</h1>
+          <span>
+            총 <strong>!!맛집!!</strong>개의 맛집이
+            <br />
+            당신을 기다리고 있어요
+          </span>
+          <button>보러가기</button>
+        </MatZip>
+      </div>
+      <HireUl></HireUl>
+    </MapBody>
+  );
+};
+
+const MapBody = styled.body`
+  display: flex;
+`;
+const SelectedMap = styled(MapContainer)`
+  width: 620px;
+  height: 413px;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 32px;
+  border: 1px solid #e5e5ec;
+  box-sizing: border-box;
+  box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.12); ;
+`;
+const MatZip = styled.div`
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #e5e5ec;
+  box-sizing: border-box;
+  border-radius: 16px;
+  padding-left: 32px;
+  position: absolute;
+  width: 290px;
+  height: 413px;
+  top: 30%;
+  left: calc(50% + 495px);
+  transform: translate(-50%, -50%);
+  h1 {
+    color: #767676;
+    font-size: 14px;
+    line-height: 16px;
+    margin-top: 111px;
+  }
+  span {
+    color: #111111;
+    font-size: 18px;
+    line-height: 26px;
+    margin-top: 16px;
+    strong {
+      font-size: 40px;
+      line-height: 47px;
+    }
+  }
+  button {
+    color: #ffffff;
+    background-color: #1d1d1d;
+    width: 145px;
+    height: 56px;
+    border: none;
+    border-radius: 62px;
+    margin-top: 33px;
+    font-size: 18px;
+    line-height: 21px;
+  }
+`;
+const HireUl = styled.ul`
+  background-color: red;
+  height: 100px;
+  width: 100px;
+`;
+
+export default NearHome;
